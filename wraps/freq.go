8bcf2ced6056d5072399b28e530d50e76cf8f790
张大千 <zhangdaqian@iqidao.com>
2020-03-18 14:56:40 +0800

    add GetCall

--- wraps/freq.go
+++ wraps/freq.go
@@ -3,6 +3,7 @@ package wraps
 import (
 	"sync"
 
+	"github.com/RocksonZeta/wrap/msclient"
 	"github.com/RocksonZeta/wrap/mysqlwrap"
 	"github.com/RocksonZeta/wrap/osswrap"
 	"github.com/RocksonZeta/wrap/rediswrap"
@@ -11,10 +12,12 @@ import (
 
 // wrap实例管理
 
+// all thread safe ,call be singleton
 var redises sync.Map
 var mysqls sync.Map
-var https sync.Map
 var osses sync.Map
+var requests sync.Map
+var calls sync.Map
 
 func GetRedis(options rediswrap.Options) *rediswrap.Redis {
 	old, ok := redises.Load(options.Url)
@@ -36,20 +39,30 @@ func GetMysql(options mysqlwrap.Options) *mysqlwrap.Mysql {
 }
 func GetOss(options osswrap.Options, bucketName string) *osswrap.Oss {
 	key := bucketName + "." + options.Endpoint
-	old, ok := mysqls.Load(key)
+	old, ok := osses.Load(key)
 	if ok {
 		return old.(*osswrap.Oss)
 	}
 	n := osswrap.New(options, bucketName)
-	mysqls.Store(key, n)
+	osses.Store(key, n)
 	return n
 }
 func GetRequest(baseUrl string, headers, cookies map[string]string, timeout int) *requestwrap.Request {
-	old, ok := mysqls.Load(baseUrl)
+	old, ok := requests.Load(baseUrl)
 	if ok {
 		return old.(*requestwrap.Request)
 	}
 	n := requestwrap.New(baseUrl, headers, cookies, timeout)
-	mysqls.Store(baseUrl, n)
+	requests.Store(baseUrl, n)
+	return n
+}
+
+func GetCall(baseUrl string, headers, cookies map[string]string, timeout int) *msclient.Call {
+	old, ok := calls.Load(baseUrl)
+	if ok {
+		return old.(*msclient.Call)
+	}
+	n := msclient.New(baseUrl, headers, cookies, timeout)
+	calls.Store(baseUrl, n)
 	return n
 }
